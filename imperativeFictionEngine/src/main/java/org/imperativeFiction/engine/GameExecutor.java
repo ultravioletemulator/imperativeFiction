d969807e0cd757b1fb0b42c1142580d6025fc491
a.alkorta <asier.alkorta@gmail.com>
2015-07-22 11:52:32 +0200

    · add pm3 player engine javaZoom jlayer

--- imperativeFictionEngine/src/main/java/org/imperativeFiction/engine/GameExecutor.java
+++ imperativeFictionEngine/src/main/java/org/imperativeFiction/engine/GameExecutor.java
@@ -1,8 +1,14 @@
 package org.imperativeFiction.engine;
 
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 import org.imperativeFiction.generated.Game;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created by developer on 7/22/15.
@@ -11,8 +17,19 @@ public class GameExecutor {
 
 	private Logger logger = LoggerFactory.getLogger(GameExecutor.class);
 
-	public void executeGame(Game game) {
+	public void executeGame(Game game) throws GameException {
+		try {
+			playMusic(new File(game.getMusic().getFile().get(0).getPath()));
+		} catch (FileNotFoundException e) {
+			throw new GameException(e);
+		} catch (JavaLayerException e) {
+			throw new GameException(e);
+		}
+	}
 
-
+	private void playMusic(File file) throws FileNotFoundException, JavaLayerException {
+		FileInputStream fis = new FileInputStream(file);
+		//Player player = new Player(fis);
+		//player.play();
 	}
 }
